eed17187ef1eb7ce1b7defe524a4f5701bb0ba7b
leandrocodes <leaxviana140@gmail.com>
2019-09-04 14:23:47 -0400

    api config

---
+++ src/api/firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyA851SWqucRdE3hWgsiBHrkNqPug_UQroM",
+    authDomain: "codecake-t2.firebaseapp.com",
+    databaseURL: "https://codecake-t2.firebaseio.com",
+    projectId: "codecake-t2",
+    storageBucket: "",
+    messagingSenderId: "111437043255",
+    appId: "1:111437043255:web:e9ac3e1ce881a8f71c51b2"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
