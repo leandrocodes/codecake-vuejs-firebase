adca4c4f754227496c4f5ae8e15f7c0d11189516
leandrocodes <leaxviana140@gmail.com>
2019-09-05 15:19:41 -0400

    fix comps

--- src/views/Cliente/Home.vue
+++ src/views/Cliente/Home.vue
@@ -1,16 +1,13 @@
 <template>
-    <div class="homeCliente">
-        <form-casamento></form-casamento>
-    </div>
+  
 </template>
 
 <script>
-import FormCasamento from '@/components/Cliente/FormCasamento'
 export default {
-    name: 'Home',
-    components: {FormCasamento}
+
 }
 </script>
 
 <style>
+
 </style>
--- src/views/Cliente/Login.vue
+++ src/views/Cliente/Login.vue
@@ -1,17 +1,92 @@
 <template>
-  <div>
-    <LoginForm/>
-  </div>
+    <div class="loginForm">
+        <vs-row>
+            <vs-col
+                vs-offset="2"
+                vs-type="flex"
+                vs-justify="center"
+                vs-align="center"
+                vs-w="8"
+                vs-xs="12"
+                vs-xs-offset="0"
+            >
+                <vs-input
+                    icon-no-border
+                    size="large"
+                    icon="account_circle"
+                    label-placeholder="E-mail"
+                    v-model="email"
+                    :color="tipo"
+                />
+            </vs-col>
+        </vs-row>
+        <vs-row>
+            <vs-col
+                vs-offset="2"
+                vs-type="flex"
+                vs-justify="center"
+                vs-align="center"
+                vs-w="8"
+                vs-xs="12"
+                vs-xs-offset="0"
+            >
+                <vs-input
+                    icon-no-border
+                    size="large"
+                    icon="lock"
+                    label-placeholder="Senha"
+                    v-model="senha"
+                    :color="tipo"
+                />
+            </vs-col>
+        </vs-row>
+        <vs-row>
+            <vs-col vs-offset="3" vs-type="flex" vs-justify="center" vs-align="center" vs-w="6">
+                <router-link to="/cadastro">
+                    <vs-button color="#9490C0" type="flat">Cadastrar</vs-button>
+                </router-link>
+                <vs-button
+                    @click="login"
+                    color="#C893AA"
+                    gradient-color-secondary="#9490C0"
+                    type="gradient"
+                >Login</vs-button>
+            </vs-col>
+        </vs-row>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import LoginForm from '@/components/Cliente/LoginForm.vue'
-
 export default {
-  name: 'Login',
-  components: {
-    LoginForm
-  }
+    data() {
+        return {
+            tipo: '#E79EBD',
+            email: '',
+            senha: ''
+        }
+    },
+    methods: {
+        login() {
+            this.$firebase
+                .auth()
+                .signInWithEmailAndPassword(this.email, this.senha)
+                .then(() => {
+                    this.$router.replace('home')
+                })
+        }
+    }
 }
 </script>
+
+<style scoped>
+.login-form {
+    padding: 10em;
+}
+.vs-input {
+    margin: 10px;
+    width: 70%;
+}
+.vs-button {
+    margin: 25px 70px;
+}
+</style>
