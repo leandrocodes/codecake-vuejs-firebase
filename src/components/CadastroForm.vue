1a13c2a9d089781a0e4a126248d55016dba4f6f7
leandrocodes <leaxviana140@gmail.com>
2019-09-04 20:22:11 -0400

    form finished

--- src/components/CadastroForm.vue
+++ src/components/CadastroForm.vue
@@ -1,45 +1,25 @@
 <template>
     <div class="cadastroForm">
-        <section>
+        <v-app id="inspire">
             <v-form v-model="valid">
                 <v-container>
                     <v-row>
-                        <v-col cols="6" md="6">
+                        <v-col cols="12" md="6">
                             <v-text-field
-                                v-model="user.firstname"
-                                :rules="userRules.nameRules"
+                                v-model="firstname"
+                                :rules="nameRules"
                                 :counter="10"
                                 label="Nome"
                                 required
                             ></v-text-field>
                         </v-col>
-                        <v-col cols="6" md="6">
+
+                        <v-col cols="12" md="6">
                             <v-text-field
-                                v-model="user.lastname"
-                                :rules="userRules.nameRules"
+                                v-model="lastname"
+                                :rules="nameRules"
                                 :counter="10"
                                 label="Sobrenome"
-                                required
-                            ></v-text-field>
-                        </v-col>
-                    </v-row>
-                    <v-row>
-                        <v-col cols="12">
-                            <v-text-field
-                                v-model="user.email"
-                                :rules="userRules.emailRules"
-                                label="Email"
-                                required
-                            ></v-text-field>
-                        </v-col>
-                    </v-row>
-                    <v-row>
-                        <v-col cols="12">
-                            <v-text-field
-                                type="password"
-                                v-model="user.password"
-                                :rules="[userRules.passRules.required, userRules.passRules.min]"
-                                label="Senha"
                                 required
                             ></v-text-field>
                         </v-col>
@@ -48,17 +28,32 @@
                     <v-row>
                         <v-col cols="12" md="12">
                             <v-text-field
-                                v-model="user.firstname"
-                                :rules="userRules.nameRules"
-                                :counter="10"
-                                label="Nome"
+                                v-model="email"
+                                :rules="emailRules"
+                                label="E-mail"
                                 required
+                            ></v-text-field>
+                        </v-col>
+                    </v-row>
+                    
+                    <v-row>
+                        <v-col cols="12" md="12">
+                            <v-text-field
+                                v-model="password"
+                                :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+                                :rules="[rules.required, rules.min]"
+                                :type="show ? 'text' : 'password'"
+                                name="input-10-1"
+                                label="Senha"
+                                hint="At least 8 characters"
+                                counter
+                                @click:append="show = !show"
                             ></v-text-field>
                         </v-col>
                     </v-row>
                 </v-container>
             </v-form>
-        </section>
+        </v-app>
     </div>
 </template>
 
@@ -66,27 +61,25 @@
 export default {
     data() {
         return {
+            show: false,
             valid: false,
-            user: {
-                firstname: "",
-                lastname: "",
-                email: "",
-                password: ""
-            },
-            userRules: {
-                nameRules: [
-                    v => !!v || "Name is required",
-                    v =>
-                        v.length <= 10 || "Name must be less than 10 characters"
-                ],
-                emailRules: [
-                    v => !!v || "E-mail is required",
-                    v => /.+@.+\..+/.test(v) || "E-mail must be valid"
-                ],
-                passRules: {
-                    required: value => !!value || "Required.",
-                    min: v => v.length >= 8 || "Min 8 characters"
-                }
+            firstname: "",
+            lastname: "",
+            nameRules: [
+                v => !!v || "Name is required",
+                v => v.length <= 10 || "Name must be less than 10 characters"
+            ],
+            email: "",
+            emailRules: [
+                v => !!v || "E-mail is required",
+                v => /.+@.+/.test(v) || "E-mail must be valid"
+            ],
+            password: "",
+            rules: {
+                required: value => !!value || "Required.",
+                min: v => v.length >= 8 || "Min 8 characters",
+                emailMatch: () =>
+                    "The email and password you entered don't match"
             }
         }
     }
